84102fa31de066465a9ac9cd67c29306f95cb6ef
aquayean <amoyyean@gmail.com>
2017-07-30 20:24:24 +0800

    Spider code through lxml

--- SpiderLesson3-lxml.py
+++ SpiderLesson3-lxml.py
@@ -1,17 +1,16 @@
 # coding: utf-8
 """
-爬虫课练习代码
+爬虫课练习代码lxml版本
 课程作业-爬虫入门03-爬虫基础-WilliamZeng-20170716
 """
 
 import os
 import time
 import urllib2
+import lxml.html # lxml中的HTML返回结果解析模块
+import lxml.etree # 为了解决中文乱码而专门引入的lxml模块
 import urlparse
-from bs4 import BeautifulSoup
-"""
-用于解析网页中文, 安装:pip install beautifulsoup4.Windows环境下需要在命令行模式里并在安装pip的目录下运行,比如D:\Python27\Scripts.PyCharm可以直接在Project Interpretr界面安装各种package.
-"""
+
 
 def download(url, retry=2):
     """
@@ -43,39 +42,37 @@ def crawl_article_images(post_url):
     抓取文章中图片链接
     :param post_url: 文章页面
     """
-    image_url = set()  # 爬取的图片链接
+    image_link = []
     flag = True # 标记是否需要继续爬取
     while flag:
-        html = download(post_url) # 下载页面
-        if html == None:
+        page = download(post_url) # 下载页面
+        if page == None:
+            break
+        my_parser = lxml.etree.HTMLParser(encoding="utf-8")
+        html_content = lxml.etree.HTML(page, parser=my_parser) # 格式化爬取的页面数据
+        # html_content = lxml.html.fromstring(page) # 格式化爬取的页面数据,fromstring函数未找到解决中文乱码的办法
+        title = html_content.xpath('//h1[@class="title"]/text()')  # 获取文章标题
+        image_link = html_content.xpath('//div/img/@data-original-src') # 获取图片的原始链接
+        image_caption = html_content.xpath('//div[@class="image-caption"]/text()')
+        if image_link.__len__() == 0: # 爬取的页面中无图片div元素，终止爬取
             break
 
-        soup = BeautifulSoup(html, "html.parser") # 格式化爬取的页面数据
-        title = soup.find('h1', {'class': 'title'}).text  # 获取文章标题
-        image_div = soup.find_all('div', {'class': 'image-package'}) # 获取文章图片div元素
-        if image_div.__len__() == 0: # 爬取的页面中无图片div元素，终止爬取
-            break
-
-        i = 1
         image_content = ''
-        for image in image_div:
-            image_link = image.img.get('data-original-src') # 获取图片的原始链接
-            image_caption = image.find('div', {'class': 'image-caption'}).text # 获取图片的标题
-            image_content += str(i) + '. ' + (unicode(image_caption).encode('utf-8', errors='ignore')) + ' : '+ (unicode(image_link).encode('utf-8', errors='ignore')) + '\n'
-            image_url.add(image_link)  # 记录未重复的爬取的图片链接
-            i += 1
+        for i in range(image_link.__len__()):
+             # 获取图片的标题
+            image_content += str(i + 1) + '. ' + (unicode(image_caption[i]).encode('utf-8', errors='ignore')) + ' : '+ image_link[i] + '\n'
 
         if os.path.exists('spider_output/') == False:  # 检查保存文件的地址
             os.mkdir('spider_output')
 
-        file_name = 'spider_output/' + title + '_images.txt'  # 设置要保存的文件名
+        file_name = 'spider_output/' + title[0] + '_images_by_lxml.txt'  # 设置要保存的文件名
         if os.path.exists(file_name) == False:
-            file = open('spider_output/' + title + '_images.txt', 'wb')  # 写文件
+            file = open('spider_output/' + title[0] + '_images_by_lxml.txt', 'wb')  # 写文件
             file.write(image_content)
             file.close()
         flag = False
 
-    image_num = image_url.__len__()
+    image_num = image_link.__len__()
     print 'total number of images in the article: ', image_num
 
 def crawl_article_text_link(post_url):
@@ -83,40 +80,35 @@ def crawl_article_text_link(post_url):
     抓取文章中的文字链接
     :param post_url: 文章页面
     """
-    text_link_url = set()  # 爬取的文字链接
     flag = True # 标记是否需要继续爬取
     while flag:
-        html = download(post_url) # 下载页面
-        if html == None:
+        page = download(post_url) # 下载页面
+        if page == None:
             break
 
-        soup = BeautifulSoup(html, "html.parser") # 格式化爬取的页面数据
-        title = soup.find('h1', {'class': 'title'}).text  # 获取文章标题
-        article_content = soup.find('div', {'class': 'show-content'}) # 获取文章的内容div
-        text_links = article_content.find_all('a', {'target': '_blank'})
+        my_parser = lxml.etree.HTMLParser(encoding="utf-8")
+        html_content = lxml.etree.HTML(page, parser=my_parser)  # 格式化爬取的页面数据
+        title = html_content.xpath('//h1[@class="title"]/text()')  # 获取文章标题
+        text_links = html_content.xpath('//div[@class="show-content"]//a/@href')
+        text_links_label = html_content.xpath('//div[@class="show-content"]//a/text()')
         if text_links.__len__() == 0: # 爬取的页面中无图片div元素，终止爬取
             break
 
-        i = 1
         text_links_content = ''
-        for link in text_links:
-            link_url = link.get('href') # 获取文字链的链接
-            link_label = link.text # 获取文字链的文本内容
-            text_links_content += str(i) + '. ' + (unicode(link_label).encode('utf-8', errors='ignore')) + ' : '+ (unicode(link_url).encode('utf-8', errors='ignore')) + '\n'
-            text_link_url.add(link_url)  # 记录未重复的爬取的文字链的链接
-            i += 1
+        for i in range(text_links.__len__()):
+            text_links_content += str(i + 1) + '. ' + (unicode(text_links_label[i]).encode('utf-8', errors='ignore')) + ' : '+ text_links[i] + '\n'
 
         if os.path.exists('spider_output/') == False:  # 检查保存文件的地址
             os.mkdir('spider_output')
 
-        file_name = 'spider_output/' + title + '_article_text_links.txt'  # 设置要保存的文件名
+        file_name = 'spider_output/' + title[0] + '_article_text_links_by_lxml.txt'  # 设置要保存的文件名
         if os.path.exists(file_name) == False:
-            file = open('spider_output/' + title + '_article_text_links.txt', 'wb')  # 写文件
+            file = open('spider_output/' + title[0] + '_article_text_links_by_lxml.txt', 'wb')  # 写文件
             file.write(text_links_content)
             file.close()
         flag = False
 
-    link_num = text_link_url.__len__()
+    link_num = text_links.__len__()
     print 'total number of text links in the article: ', link_num
 
 def crawl_page(crawled_url):
@@ -125,22 +117,25 @@ def crawl_page(crawled_url):
     :param crawled_url: 需要爬取的页面地址集合
     """
     for link in crawled_url: #按地址逐篇文章爬取
-        html = download(link)
-        soup = BeautifulSoup(html, "html.parser")
-        title = soup.find('h1', {'class': 'title'}).text #获取文章标题
-        content = soup.find('div', {'class': 'show-content'}).text #获取文章内容
+        page = download(link)
+        my_parser = lxml.etree.HTMLParser(encoding="utf-8")
+        html_content = lxml.etree.HTML(page, parser=my_parser)
+        title = html_content.xpath('//h1[@class="title"]/text()') #获取文章标题
+        contents = html_content.xpath('//div[@class="show-content"]//text()') #获取文章内容
+        content = ''.join(contents)
 
         if os.path.exists('spider_output/') == False: #检查保存文件的地址
             os.mkdir('spider_output/')
 
-        file_name = 'spider_output/' + title + '.txt' #设置要保存的文件名
+        file_name = 'spider_output/' + title[0] + '_by_lxml.txt' #设置要保存的文件名
         if os.path.exists(file_name):
             # os.remove(file_name) # 删除文件
             continue  # 已存在的文件不再写
-        file = open('spider_output/' + title + '.txt', 'wb') #写文件
+        file = open('spider_output/' + title[0] + '_by_lxml.txt', 'wb') #写文件
         content = unicode(content).encode('utf-8', errors='ignore')
         file.write(content)
         file.close()
+
 
 crawl_article_images('http://www.jianshu.com/p/10b429fd9c4d')
 crawl_article_images('http://www.jianshu.com/p/faf2f4107b9b')
